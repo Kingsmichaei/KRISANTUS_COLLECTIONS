51fcac86a706dd70419f48980b3de75218cdd4e1
Kings Michael <kingsmichael.official@gmail.com>
2026-09-16 12:56:30 +0100

    Project setup, dependency installation, folder structure, env

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Services from './pages/Services'
+import Showcase from './pages/Showcase'
+import Contact from './pages/Contact'
+import AdminLogin from './pages/AdminLogin'
+import AdminDashboard from './pages/AdminDashboard'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/showcase" element={<Showcase />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
